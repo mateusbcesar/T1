b2b4cc5f6dc394f0f12c1d0a854d177f4ff74a52
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-02 18:54:54 -0300

    Empregado.java

--- Projeto/Employee.java
+++ Projeto/Employee.java
@@ -1,14 +1,8 @@
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+public class Empregado extends Usuário {
+    private Departamento departamento;
 
-// Classe para representar um funcionário
-class Employee extends User {
-    private Department department;
-
-    public Employee(int userId, String userName, Department department) {
-        super(userId, userName, UserType.EMPLOYEE);
-        this.department = department;
+    public Empregado(int idUsuario, String nomeUsuario, Departamento departamento, UserType tipoUsuario) {
+        super(idUsuario, nomeUsuario, tipoUsuario.EMPLOYEE);
+        this.departamento = departamento;
     }
-
 }
