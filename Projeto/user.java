f44a885b59fe37131b5eb8d4d8ca852f553f316e
Mateus César <134755701+mateusbcesar@users.noreply.github.com>
2023-10-02 16:30:35 -0300

    Create user.java

---
+++ Projeto/user.java
@@ -0,0 +1,14 @@
+// Classe para representar um usuário
+class User {
+    private int userId;
+    private String userName;
+    private UserType userType;
+
+    public User(int userId, String userName, UserType userType) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userType = userType;
+    }
+
+    // Métodos getters e setters
+}
