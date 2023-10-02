45cf3ef9a9a57b6076e5c3a3593caef552868757
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-02 18:55:58 -0300

    ComprarItem.java

--- Projeto/PurchaseOrder.java
+++ Projeto/PurchaseOrder.java
@@ -2,29 +2,28 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-// Classe para representar um pedido de aquisição
-class PurchaseOrder {
-    private Employee requester;
-    private Department requestingDepartment;
-    private Date orderDate;
-    private Date completionDate;
-    private OrderStatus orderStatus;
-    private List<OrderItem> orderItems;
+public class ComprarItem{
 
-    public PurchaseOrder(Employee requester, Department requestingDepartment, Date orderDate) {
-        this.requester = requester;
-        this.requestingDepartment = requestingDepartment;
-        this.orderDate = orderDate;
-        this.orderStatus = OrderStatus.OPEN;
-        this.orderItems = new ArrayList<>();
+
+    private Empregado empregado;
+    private Departamento departamentoEmpregado;
+    private Date dia;
+    private Date diaFinal;
+    private OrderStatus status;
+    private ArrayList<Item> items;
+
+    public ComprarItem(Empregado empregado, Departamento departamentoEmpregado, Date dia) {
+        this.empregado = empregado;
+        this.departamentoEmpregado = departamentoEmpregado;
+        this.dia = dia;
+        this.status = OrderStatus.OPEN;
+        this.items = new ArrayList<>();
     }
 
     // Métodos getters e setters
 
     // Método para adicionar um item ao pedido
-    public void addItem(OrderItem item) {
-        orderItems.add(item);
+    public void addItem(Item item) {
+        items.add(item);
     }
-
-    // Outros métodos relevantes
 }
