8360eee0139d804634d0977207475c03a7419bb9
Mateus César <134755701+mateusbcesar@users.noreply.github.com>
2023-10-02 16:36:30 -0300

    Create OrderItem.java

---
+++ Projeto/OrderItem.java
@@ -0,0 +1,14 @@
+// Classe para representar um item do pedido
+class OrderItem {
+    private String description;
+    private double unitPrice;
+    private int quantity;
+
+    public OrderItem(String description, double unitPrice, int quantity) {
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+
+    // Métodos getters
+}
