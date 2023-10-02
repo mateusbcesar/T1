fd5aa27ab01329b0d5dd6b0d416b46996bde74cc
Mateus César <134755701+mateusbcesar@users.noreply.github.com>
2023-10-02 16:35:28 -0300

    Create PurchaseOrder.java

---
+++ Projeto/PurchaseOrder.java
@@ -0,0 +1,26 @@
+// Classe para representar um pedido de aquisição
+class PurchaseOrder {
+    private Employee requester;
+    private Department requestingDepartment;
+    private Date orderDate;
+    private Date completionDate;
+    private OrderStatus orderStatus;
+    private List<OrderItem> orderItems;
+
+    public PurchaseOrder(Employee requester, Department requestingDepartment, Date orderDate) {
+        this.requester = requester;
+        this.requestingDepartment = requestingDepartment;
+        this.orderDate = orderDate;
+        this.orderStatus = OrderStatus.OPEN;
+        this.orderItems = new ArrayList<>();
+    }
+
+    // Métodos getters e setters
+
+    // Método para adicionar um item ao pedido
+    public void addItem(OrderItem item) {
+        orderItems.add(item);
+    }
+
+    // Outros métodos relevantes
+}
