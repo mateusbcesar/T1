dd70582ba5f950ef8356ffb2993947ea297a6ccc
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-18 00:04:57 -0300

    Update Usuário.java

--- Projeto/Usuário.java
+++ Projeto/Usuário.java
@@ -1,11 +1,37 @@
-public class Usuário {
+package Projeto;
+
+public class Usuario {
     private int idUsuario;
     private String nomeUsuario;
-    private UserType tipoUsuario;
+    private TipoDeUsuário tipoUsuario;
 
-    public Usuário(int idUsuario, String nomeUsuario, UserType tipoUsuario) {
+    public Usuario(int idUsuario, String nomeUsuario, TipoDeUsuário tipoUsuario) {
         this.idUsuario = idUsuario;
         this.nomeUsuario = nomeUsuario;
         this.tipoUsuario = tipoUsuario;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public TipoDeUsuário getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setIdUsuario(int novoIdUsuario) {
+        this.idUsuario = novoIdUsuario;
+    }
+
+    public void setNomeUsuario(String novoNomeUsuario) {
+        this.nomeUsuario = novoNomeUsuario;
+    }
+
+    public void setTipoUsuario(TipoDeUsuário novoTipoUsuario) {
+        this.tipoUsuario = novoTipoUsuario;
     }
 }
