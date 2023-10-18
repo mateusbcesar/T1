ae603feb6966ed235470ca382749c1a4aab60a27
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-18 02:27:57 -0300

    Update ComprarItem.java

--- Projeto/ComprarItem.java
+++ Projeto/ComprarItem.java
@@ -1,7 +1,6 @@
 package Projeto;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class ComprarItem{
@@ -9,23 +8,50 @@ public class ComprarItem{
 
     private Funcionario funcionario;
     private Departamento departamentoFuncionario;
-    private Date dia;
-    private Date diaFinal;
+    private String dia;
+    private String diaFinal;
     private OrderStatus status;
     private ArrayList<Item> items;
 
-    public ComprarItem(Funcionario funcionario, Departamento departamentoFuncionario, Date dia) {
+    public ComprarItem(Funcionario funcionario, String dia) {
         this.funcionario = funcionario;
-        this.departamentoFuncionario = departamentoFuncionario;
+        this.departamentoFuncionario = funcionario.getDepartamento();
         this.dia = dia;
         this.status = OrderStatus.OPEN;
         this.items = new ArrayList<>();
     }
 
-    // Métodos getters e setters
-
-    // Método para adicionar um item ao pedido
     public void addItem(Item item) {
         items.add(item);
     }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setDiaFinal(String diaFinal) {
+        this.diaFinal = diaFinal;
+    }
+
+    public void getPrecoTotalPedido(){
+        double valorTotal = 0;
+        for (Item item : items) {
+            valorTotal += item.getPreco();
+        }
+        if(valorTotal<=departamentoFuncionario.getMaxPreco()){
+            System.out.println("Valor total do pedido: " + valorTotal);
+        }else
+            System.out.println("Valor total do pedido: " + valorTotal + " - Pedido excedeu o valor máximo permitido");
+    }
+
+    public void mostrarPedidos(){
+        for(Item item: items){
+            System.out.println("Item: " + item.getDescricao() + " - Preço: " + item.getPreco() + " - Quantidade: " + item.getQuantidade());
+        }
+    }
+
+    public String toString() {
+        return "Funcionário: " + funcionario.getNomeUsuario() + " - Departamento: " + departamentoFuncionario.getNomeDepartamento() + " - Dia: " + dia + " - Dia final: " + diaFinal + " - Status: " + status;
+    }
 }
+
