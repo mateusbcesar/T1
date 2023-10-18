8358fc90454231a1d55df0d57418268e7079d494
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-18 00:04:01 -0300

    Update Item.java

--- Projeto/Item.java
+++ Projeto/Item.java
@@ -1,3 +1,5 @@
+package Projeto;
+
 public class Item {
     private String descricao;
     private double preco;
@@ -6,6 +8,34 @@ public class Item {
     public Item(String descricao, double preco, int quantidade) {
         this.descricao = descricao;
         this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+
+    public double getPrecoTotal(){
+        return this.preco * this.quantidade;
+    }
+
+    public void setPreco(){
+        this.preco = preco;
+    }
+
+    public void setDescricao(){
+        this.descricao = descricao;
+    }
+
+    public void setQuantidade(){
         this.quantidade = quantidade;
     }
 }
