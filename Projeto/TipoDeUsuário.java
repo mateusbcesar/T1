52248a51f026fe80d18dc0367f395394140347b0
Nicholas Ástor Barbosa Navarro <nicholas.barbosa@edu.pucrs.br>
2023-10-18 02:29:39 -0300

    Update TipoDeUsuário.java

--- Projeto/TipoDeUsuário.java
+++ Projeto/TipoDeUsuário.java
@@ -1,5 +1,7 @@
 package Projeto;
 
+import java.text.Normalizer;
+
 public class TipoDeUsuário {
     private String funcionario;
     private String administrador;
@@ -9,13 +11,13 @@ public class TipoDeUsuário {
         this.administrador = null;
     }
 
-    public void setTipoUsuário(String tipoDeUsuário) {
+    /*public void setTipoUsuário(String tipoDeUsuário) {
         if (tipoDeUsuário == "funcionario") {
             this.funcionario = tipoDeUsuário;
         } else if (tipoDeUsuário == "administrador") {
             this.administrador = tipoDeUsuário;
         }
-    }
+    }*/
 
     public String getTipoDeUsuário() {
         if (this.funcionario != null) {
@@ -25,5 +27,19 @@ public class TipoDeUsuário {
         } else {
             return null;
         }
+    }
+
+    public void setTipoUsuário(String tipo){
+        String tipoNormalizado = Normalizer.normalize(tipo, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+
+        if ("funcionario".equalsIgnoreCase(tipoNormalizado)) {
+            this.funcionario = tipo;
+        } else if ("administrador".equalsIgnoreCase(tipoNormalizado)) {
+            this.administrador = tipo;
+        }
+    }
+
+    public String toString() {
+        return "Tipo de usuário: " + getTipoDeUsuário();
     }
 }
