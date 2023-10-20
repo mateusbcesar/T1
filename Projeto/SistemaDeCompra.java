6d975b9f2fe913d30df8c21f9ce5e83bd85169b1
João Dotti <143226859+JoaoDotti@users.noreply.github.com>
2023-10-20 15:09:28 -0300

    Update SistemaDeCompra.java

--- Projeto/SistemaDeCompra.java
+++ Projeto/SistemaDeCompra.java
@@ -14,12 +14,14 @@ public class SistemaDeCompra {
     public static void main(String[] args) {
         TipoDeUsuário t1 = new TipoDeUsuário();
         t1.setTipoUsuário("funcionario");
+        TipoDeUsuário t2 = new TipoDeUsuário();
+        t2.setTipoUsuário("administrador");
         Departamento d1 = new Departamento("Financeiro", 20);
         Departamento d2 = new Departamento("Manutenção", 35);
         Departamento d3 = new Departamento("Engenharia", 70);
         Departamento d4 = new Departamento("Judiciário", 15);
         Departamento d5 = new Departamento("RH", 5);
-        Funcionario f1 = new Funcionario(21, "Nicholas", d1, t1);
+        Funcionario f1 = new Funcionario(21, "Nicholas", d1, t2);
         Funcionario f2 = new Funcionario(95, "Maria", d2, t1);
         Funcionario f3 = new Funcionario(84, "Joao", d3, t1);
         Funcionario f4 = new Funcionario(83, "Mateus", d4, t1);
@@ -33,13 +35,13 @@ public class SistemaDeCompra {
         Funcionario f12 = new Funcionario(98, "Emma", d2, t1);
         Funcionario f13 = new Funcionario(100, "Lucas", d3, t1);
         Funcionario f14 = new Funcionario(324, "Douglas", d4, t1);
-        Funcionario f15 = new Funcionario(453, "Ana", d5, t1);
+        Funcionario f15 = new Funcionario(453, "Ana", d5, t2);
         Item i1 = new Item("Caneta", 1.50, 1);
         Item i2 = new Item("Lapis", 0.50, 1);
         Item i3 = new Item("Borracha", 0.75, 1);
         Item i4 = new Item("Caderno", 5.00, 1);
         Item i5 = new Item("Mochila", 10.00, 1);
-        ComprarItem compra1 = new ComprarItem(f1, "01/01/2021");
+        ComprarItem compra1 = criarNovoPedido(f1, "01/01/2021");
         compra1.addItem(i5);
         compra1.addItem(i4);
         compra1.mostrarPedidos();
@@ -53,6 +55,13 @@ public class SistemaDeCompra {
         compra.mostrarItens();
         compra.getPrecoTotalPedido();
         System.out.println(compra);
+
+        avaliarPedido(compra, OrderStatus.APPROVED);
+        avaliarPedido(compra1, OrderStatus.APPROVED);
+        exibirEstatisticasGerais(List.of(compra1, compra));
+
+        compra.buscarPedidosPorFuncionario(List.of(compra, compra1), f15);
+        compra.listarPedidosEntreDatas(List.of(compra, compra1), "01/01/2021", "05/01/2021");
     }
 
     private static ComprarItem criarNovoPedido(Funcionario funcionario, String dia){
