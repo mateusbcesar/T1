0de3623bdea7704da45de4f2c050e7e7f632dbbc
Mateus César <134755701+mateusbcesar@users.noreply.github.com>
2023-10-20 16:34:41 -0300

    Update ComprarItem.java

--- Projeto/ComprarItem.java
+++ Projeto/ComprarItem.java
@@ -1,7 +1,6 @@
 package Projeto;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class ComprarItem{
@@ -9,23 +8,117 @@ public class ComprarItem{
 
     private Funcionario funcionario;
     private Departamento departamentoFuncionario;
-    private Date dia;
-    private Date diaFinal;
+    private String dia;
+    private String diaFinal;
     private OrderStatus status;
     private ArrayList<Item> items;
 
-    public ComprarItem(Funcionario funcionario, Departamento departamentoFuncionario, Date dia) {
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public ComprarItem(Funcionario funcionario, String dia) {
         this.funcionario = funcionario;
-        this.departamentoFuncionario = departamentoFuncionario;
+        this.departamentoFuncionario = funcionario.getDepartamento();
         this.dia = dia;
         this.status = OrderStatus.OPEN;
         this.items = new ArrayList<>();
     }
 
-    // Métodos getters e setters
+    public OrderStatus getStatus() {
+        return status;
+    }
 
-    // Método para adicionar um item ao pedido
     public void addItem(Item item) {
         items.add(item);
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setDiaFinal(String diaFinal) {
+        this.diaFinal = diaFinal;
+    }
+
+    public void getPrecoTotalPedido(){
+        double valorTotal = 0;
+        for (Item item : items) {
+            valorTotal += item.getPreco();
+        }
+        if(valorTotal<=departamentoFuncionario.getMaxPreco()){
+            System.out.println("Valor total do pedido: " + valorTotal);
+        }else
+            System.out.println("Valor total do pedido: " + valorTotal + " - Pedido excedeu o valor máximo permitido");
+    }
+
+    public void mostrarPedidos(){
+        for(Item item: items){
+            System.out.println("Item: " + item.getDescricao() + " - Preço: " + item.getPreco() + " - Quantidade: " + item.getQuantidade());
+        }
+    }
+
+    public String toString() {
+        return "Funcionário: " + funcionario.getNomeUsuario() + " - Departamento: " + departamentoFuncionario.getNomeDepartamento() + " - Dia: " + dia + " - Dia final: " + diaFinal + " - Status: " + status;
+    }
+
+    public void mostrarItens() {
+        System.out.println("Itens do Pedido:");
+        for (Item item : items) {
+            System.out.println("Descrição: " + item.getDescricao() +
+                    " | Valor Unitário: " + item.getPreco() +
+                    " | Quantidade: " + item.getQuantidade() +
+                    " | Total do Item: " + item.getPrecoTotal());
+        }
+    }
+
+    public Funcionario getFuncionario(){
+        return funcionario;
+    }
+
+    public String getDia(){
+        return dia;
+    }
+
+    public void listarPedidosEntreDatas(List<ComprarItem> pedidos, String dataInicio, String dataFim) {
+        System.out.println("Pedidos entre " + dataInicio + " e " + dataFim + ":");
+        for (ComprarItem pedido : pedidos) {
+            if (pedido.getDia().compareTo(dataInicio) >= 0 && pedido.getDia().compareTo(dataFim) <= 0) {
+                System.out.println(pedido);
+            }
+        }
+    }
+
+    public void buscarPedidosPorFuncionario(List<ComprarItem> pedidos, Funcionario funcionario) {
+        System.out.println("Pedidos de " + funcionario.getNomeUsuario() + ":");
+        for (ComprarItem pedido : pedidos) {
+            if (pedido.getFuncionario().equals(funcionario)) {
+                System.out.println(pedido);
+            }
+        }
+    }
+
+    public void buscarPedidosPorDescricaoItem(List<ComprarItem> pedidos, String descricaoItem) {
+        System.out.println("Pedidos contendo o item '" + descricaoItem + "':");
+        for (ComprarItem pedido : pedidos) {
+            for (Item item : pedido.getItems()) {
+                if (item.getDescricao().equalsIgnoreCase(descricaoItem)) {
+                    System.out.println(pedido);
+                    break;
+                }
+            }
+        }
+    }
+
+    public boolean podeSerModificado() {
+        return status == OrderStatus.OPEN;
+    }
+
+    public void excluirPedido(Funcionario funcionarioQueExclui) {
+        if (status == OrderStatus.OPEN && funcionarioQueExclui.equals(funcionario)) {
+            System.out.println("Pedido excluído com sucesso.");
+        } else {
+            System.out.println("Não é possível excluir o pedido. Verifique o status ou o usuário.");
+        }
     }
 }
