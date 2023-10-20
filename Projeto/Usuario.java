1530010530651ae198da2b428d78f8d44e6cc685
Mateus César <134755701+mateusbcesar@users.noreply.github.com>
2023-10-20 16:37:58 -0300

    Update Usuario.java

--- Projeto/Usuario.java
+++ Projeto/Usuario.java
@@ -10,4 +10,28 @@ public class Usuario {
         this.nomeUsuario = nomeUsuario;
         this.tipoUsuario = tipoUsuario;
     }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public TipoDeUsuário getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setIdUsuario(int novoIdUsuario) {
+        this.idUsuario = novoIdUsuario;
+    }
+
+    public void setNomeUsuario(String novoNomeUsuario) {
+        this.nomeUsuario = novoNomeUsuario;
+    }
+
+    public void setTipoUsuario(TipoDeUsuário novoTipoUsuario) {
+        this.tipoUsuario = novoTipoUsuario;
+    }
 }
