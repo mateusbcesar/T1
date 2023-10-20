5a23771a3337e972bc57cd53d743353312484e26
joaovictorcardosobarbosa <142632439+joaovictorcardosobarbosa@users.noreply.github.com>
2023-10-20 16:35:25 -0300

    Update Item.java

--- Projeto/Item.java
+++ Projeto/Item.java
@@ -4,10 +4,32 @@ public class Item {
     private String descricao;
     private double preco;
     private int quantidade;
+    private String Categoria;
+
 
     public Item(String descricao, double preco, int quantidade) {
         this.descricao = descricao;
         this.preco = preco;
         this.quantidade = quantidade;
+    }
+
+    public double getPreco(){
+        return this.preco;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public int getQuantidade(){
+        return this.quantidade;
+    }
+
+    public double getPrecoTotal(){
+        return this.preco * this.quantidade;
+    }
+
+    public String getCategoria() {
+        return null;
     }
 }
